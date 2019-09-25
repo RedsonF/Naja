6a9934497c2f2925e4a8d0a59c092ad93bd4f643
REDSON FARIAS BARBOSA FILHO 116110895 <redsonfbf@lcc3-107.lcc.ufcg.edu.br>
2019-09-25 10:33:56 -0300

    .

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 
 const server = express();
 
-//server.use(express.json());
-//server.use(routes);
+server.use(express.json());
+server.use(routes);
 
 server.get('/nome', (req, res) => {
     return res.send("redson");

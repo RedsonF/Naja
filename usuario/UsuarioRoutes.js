320990a86ba73e88f0675161257ff4ed5978c83d
Redson <redson.filho@ccc.ufcg.edu.br>
2019-10-02 19:19:16 -0300

    .

--- usuario/UsuarioRoutes.js
+++ usuario/UsuarioRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 const usuarioController = require('./UsuarioController');
 const passport = require('passport');
 const passportConf = require('../passport');
 const passportGoogle = passport.authenticate('googleToken', { session: false });
 
-//router.route('/oauth/google')
- // .post(passportGoogle, usuarioController.googleOAuth);
+router.route('/oauth/google')
+  .post(passportGoogle, usuarioController.googleOAuth);
 
-module.exports = router;
+module.exports = router;*/
